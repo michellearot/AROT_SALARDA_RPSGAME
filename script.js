958e1258442a7b2dc2665dbabfeda5ddf118d2f3
michellearot <126306543+michellearot@users.noreply.github.com>
2023-05-10 22:53:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let [computerScore,userScore]=[0,0];
+let result_ref = document.getElementById("result");
+let choices_object = {
+    'rock' : {
+        'rock' : 'draw',
+        'scissor' : 'win',
+        'paper' : 'lose'
+    },
+    'scissor' : {
+        'rock' : 'lose',
+        'scissor' : 'draw',
+        'paper' : 'win'
+    },
+    'paper' : {
+        'rock' : 'win',
+        'scissor' : 'lose',
+        'paper' : 'draw'
+    }
+
+}
+
+function checker(input){
+    var choices = ["rock", "paper", "scissor"];
+    var num = Math.floor(Math.random()*3);
+
+    document.getElementById("compChoice").innerHTML = 
+    ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
+
+    document.getElementById("userChoice").innerHTML = 
+    ` You choose <span> ${input.toUpperCase()} </span>`;
+
+    let computer_choice = choices[num];
+
+    switch(choices_object[input][computer_choice]){
+        case 'win':
+            result_ref.style.cssText = "background-color: #cefdce; color: #689f38";
+            result_ref.innerHTML = "YOU WIN";
+            userScore++;
+            break;
+        case 'lose':
+            result_ref.style.cssText = "background-color: #ffdde0; color: #d32f2f";
+            result_ref.innerHTML = "YOU LOSE";
+            computerScore++;
+            break;
+        default:
+            result_ref.style.cssText = "background-color: #e5e5e5; color: #808080";
+            result_ref.innerHTML = "DRAW";
+            break;
+    }
+
+    document.getElementById("computerScore").innerHTML = computerScore;
+    document.getElementById("userScore").innerHTML = userScore;
+}
